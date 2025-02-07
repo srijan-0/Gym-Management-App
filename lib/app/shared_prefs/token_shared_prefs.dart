a5b443442059c2bf7dad6989bdbd9f5dee13d753
srijan-0 <srijan.shrestha000@gmail.com>
2025-02-07 14:55:05 +0545

    updating

---
+++ lib/app/shared_prefs/token_shared_prefs.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:login/core/error/failure.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenSharedPrefs {
+  final SharedPreferences _sharedPreferences;
+
+  TokenSharedPrefs(this._sharedPreferences);
+
+  Future<Either<Failure, Unit>> saveToken(String token) async {
+    try {
+      await _sharedPreferences.setString('token', token);
+      return Right(unit);
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, String>> getToken() async {
+    try {
+      final token = _sharedPreferences.getString('token');
+      return Right(token ?? '');
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+}
